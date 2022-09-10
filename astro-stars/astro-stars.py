0d3fc899a070814b924ba86cb6311da7c72d0752
François Guillemé <francois.guilleme@parrot.com>
2022-09-10 18:35:47 +0200

    use sep source extraction (more accurate)

--- astro-stars/astro-stars.py
+++ astro-stars/astro-stars.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from struct import unpack
+import math
 import gi
 gi.require_version('Gimp', '3.0')
 from gi.repository import Gimp
@@ -16,8 +17,9 @@ from bsz_gimp_lib import PlugIn, ParamCombo, ParamString, ParamNumber, ParamBool
 
 from sparkles import *
 
+@timeit
 def save_image(image, drawable, file_path):
-    interlace, compression = 0, 2
+    interlace, compression = 0, 0
     Gimp.get_pdb().run_procedure(
         "file-png-save",
         [
@@ -41,7 +43,8 @@ def save_image(image, drawable, file_path):
         ],
     )
 
-def create_sparkles(image, drawable, name, fwhm, count, angle, size, max_opacity, th, sigma, acc):
+@timeit
+def create_sparkles(image, drawable, name, count, size1, size2, angle):
     # Gimp.context_push()
     image.undo_group_start()
     Gimp.progress_init('Detect stars...')
@@ -56,34 +59,25 @@ def create_sparkles(image, drawable, name, fwhm, count, angle, size, max_opacity
     if sparkles is None:
         sparkles = Gimp.Layer.new(image, name,
                          tmp.get_width(), tmp.get_height(),
-                         type, 10,
+                         type, 20,
                          Gimp.LayerMode.NORMAL)
         sparkles.fill(Gimp.FillType.TRANSPARENT)
         image.insert_layer(sparkles, None, 0)
-    if not acc:
-        sparkles.fill(Gimp.FillType.TRANSPARENT)
+    sparkles.fill(Gimp.FillType.TRANSPARENT)
 
     Gimp.progress_pulse()
     # save layer as an image
     path = '/tmp/sparkles.png'
     save_image(image, tmp, path)
 
-    stars = detect_stars(path, count=count, fwhm=fwhm, sigma=sigma, th=th)
-    smallest = min([s[2] for s in stars])
-    biggest = max([s[2] for s in stars])
-    print(smallest, biggest)
+    stars = detect_stars(path, count=count)
+    ff = [f for x, y, f in stars]
+    mx = max(ff)
     for x, y, flux in stars:
+        flux = math.sqrt((flux/mx))
         Gimp.progress_pulse()
-        rc=Gimp.context_set_brush('sparkle')
-        if flux == smallest:
-            bsize = 1
-            opacity = 1
-        else:
-            ratio = (2*(flux-smallest) / (biggest-smallest))**2
-            bsize = int(ratio*size/2)
-            if bsize == 0:
-                bsize = 1
-            opacity = max(1, min(max_opacity, int(ratio * max_opacity)))
+        bsize =  size1 + (size2 - size1)  * flux
+        opacity = min(100, max(5, round(100*flux)))
         Gimp.context_set_brush_size(bsize)
         Gimp.context_set_opacity(opacity)
         Gimp.context_set_paint_mode(Gimp.LayerMode.NORMAL)
@@ -98,9 +92,8 @@ def create_sparkles(image, drawable, name, fwhm, count, angle, size, max_opacity
         _color = Gimp.RGB()
         _color.set(rgb[0], rgb[1], rgb[2])
         Gimp.context_set_foreground(_color)
-        Gimp.paintbrush_default(sparkles, [x, y])
+        # Gimp.paintbrush_default(sparkles, [x, y])
 
-    # drawable.update(x, y, width, height)
     Gimp.displays_flush()
 
     Gimp.progress_end()
@@ -112,14 +105,10 @@ plugin = PlugIn(
     "Star sparkles",  # name
     create_sparkles,
     ParamString("Layer name", "sparkles"),
-    ParamNumber("FWMH", 7, 1, 30, ui_step=0.5),
     ParamNumber("Count", 500, 10, 2000, ui_step=10),
+    ParamNumber("Minimum Size", 50, 50, 200, ui_step=10),
+    ParamNumber("Maximum Size", 200, 50, 1000, ui_step=50),
     ParamNumber("Angle", 35, 0, 180, ui_step=5),
-    ParamNumber("Size", 50, 10, 1000, ui_step=5),
-    ParamNumber("Max opacity", 20, 10, 100, ui_step=1),
-    ParamNumber("Threshold", 1, 1, 15, ui_step=1),
-    ParamNumber("Sigma", 2, 1, 5, ui_step=1),
-    ParamBool("Accumulative", True),
     description="Add star sparkles",
     images="RGB*, GRAY*",
     path = "<Image>/Astro/",
--- astro-stars/sparkles.py
+++ astro-stars/sparkles.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from time import time
 
 # hack to grab PYTHONPATH from .config/GIMP/2.99/environ/python.env
 if 'PYTHONPATH' in os.environ:
@@ -7,21 +8,38 @@ if 'PYTHONPATH' in os.environ:
 
 import numpy as np
 from PIL import Image
+import sep
 
-from astropy.stats import sigma_clipped_stats
-from photutils.detection import DAOStarFinder
-from photutils.detection import find_peaks
+def timeit(func):
+    # This function shows the execution time of 
+    # the function object passed
+    def wrap_func(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
+        return result
+    return wrap_func
 
-def detect_stars(path, count=20, fwhm=3.0, th=5.0, sigma=3):
+@timeit
+def detect_stars(path, count):
     image = Image.open(path).convert('L')
-    data = np.asarray(image)
+    data = np.asarray(image).copy().astype(np.float64)
 
-    mean, median, std = sigma_clipped_stats(data, sigma=sigma)
-    daofind = DAOStarFinder(fwhm=fwhm, threshold=th*std, brightest=count)
-    sources = daofind(data - median)
-    positions = np.transpose((sources['xcentroid'], sources['ycentroid'], sources['flux']))
-    return positions
+    # m, s = np.mean(data), np.std(data)
+    bkg = sep.Background(data)
+    data_sub = data - bkg
+    objects = sep.extract(data_sub, 1.5, err=bkg.globalrms)
+
+    # res = [[int(o['x']), int(o['y']), round(math.sqrt(o['flux']),2)] for o in objects if o['flux'] > 100]
+    res = [[int(o['x']), int(o['y']), round(o['npix'],2)] for o in objects]
+    res = sorted(res, key=lambda a: a[2], reverse=True)
+    while len(res) > 1 and res[0][2] > res[1][2]*2:
+        res = res[1:]
+
+    res = res[:int(count)]
+    return [[x,y,n] for x,y,n in res]
 
 if __name__ == '__main__':
-    print(detect_stars(sys.argv[1], 20))
+    print(detect_stars(*sys.argv[1:]))
 
